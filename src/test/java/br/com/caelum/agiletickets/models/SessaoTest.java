fa66787f01b1f2cbd79d4210a676650e9d8579ed
erasmo.monteiro <erasmo.monteiro@gmail.com>
2016-12-13 17:49:50 -0200

    Correção do método ingresso

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -38,4 +38,11 @@ public class SessaoTest {
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
 	
+	@Test
+	public void deveVender2IngressoSeHa2Vagas() {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(2);
+		Assert.assertTrue(sessao.podeReservar(2));
+	}
+	
 }
